5cc735231fef8ef5a41ca1cf590cd8a4d9bd8c0a
Yurifag <jannesgrzebien@gmail.com>
2015-07-10 16:44:09 +0200

    initial release

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace osu_export
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(417, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Installation path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(395, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 20);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(395, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(33, 20);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(293, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(293, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 107);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(416, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 139);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "osu!export";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Media;
+using Microsoft.Win32;
+
+namespace osu_export
+{
+    public partial class Form1 : Form
+    {
+
+        private string installDir;
+        private string InstallDir
+        {
+            get
+            {
+                return installDir;
+            }
+            set
+            {
+                if (installDir != value)
+                {
+                    installDir = value;
+                    textBox1.Text = value;
+                }
+            }
+        }
+
+        private string outputDir;
+        private string OutputDir
+        {
+            get
+            {
+                return outputDir;
+            }
+            set
+            {
+                if (outputDir != value)
+                {
+                    outputDir = value;
+                    textBox2.Text = value;
+                }
+            }
+        }
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            bool foundosuinstallation = false;
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+            foreach (string subkey_name in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(subkey_name);
+                if ((string)subkey.GetValue("DisplayName") == "osu!")
+                {
+                    foundosuinstallation = true;
+                    string uninstallstring = subkey.GetValue("UninstallString").ToString();
+                    InstallDir = Path.GetDirectoryName(uninstallstring.Remove(uninstallstring.Length - 11));
+                }
+            }
+
+            if (!foundosuinstallation)
+            {
+                MessageBox.Show("It seems like osu! isn't installed on this machine. \nInstall osu! first or try setting the installation directory manually!", "osu!export error", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+            }
+
+            OutputDir = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + @"\osu!export";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            disableGUI();
+            Thread thread = new Thread(new ThreadStart(WorkThread));
+            thread.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                InstallDir = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                OutputDir = folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        private void disableGUI()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void enableGUI()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+        }
+
+        private void WorkThread()
+        {
+            if (Directory.Exists(InstallDir + @"\Songs"))
+            {
+                string[] directories = Directory.GetDirectories(InstallDir + @"\Songs");
+                if (directories.Length > 0)
+                {
+                    if (!Directory.Exists(OutputDir))
+                    {
+                        Directory.CreateDirectory(OutputDir);
+                    }
+
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        progressBar1.Value = 0;
+                        progressBar1.Maximum = directories.Length;
+                    });
+
+                    foreach (string directory in directories)
+                    {
+                        string beatmapfile = Directory.GetFiles(directory, "*.osu")[0];
+                        string[] lines = File.ReadAllLines(beatmapfile);
+                        Dictionary<string, string> metadata = new Dictionary<string, string>();
+                        metadata.Add("filename", "");
+                        metadata.Add("fileextension", "");
+                        metadata.Add("title", "");
+                        metadata.Add("artist", "");
+                        metadata.Add("artwork", "");
+                        foreach (string line in lines)
+                        {
+                            if (line.Contains("AudioFilename: "))
+                            {
+                                metadata["filename"] = line.Substring(line.LastIndexOf(":") + 2);
+                                metadata["fileextension"] = metadata["filename"].Substring(metadata["filename"].LastIndexOf("."));
+                            }
+
+                            if (line.Contains("Title:"))
+                            {
+                                metadata["title"] = line.Substring(line.LastIndexOf(":") + 1).Replace(" (TV Size)", "");
+                            }
+
+                            if (line.Contains("Artist:"))
+                            {
+                                metadata["artist"] = line.Substring(line.LastIndexOf(":") + 1);
+                            }
+
+                            if (File.Exists(directory + @"\bg.jpg"))
+                            {
+                                metadata["artwork"] = directory + @"\bg.jpg";
+                            }
+                        }
+
+                        try
+                        {
+                            File.Copy(directory + @"\" + metadata["filename"], OutputDir + @"\" + metadata["title"] + metadata["fileextension"], true);
+                            TagLib.File copiedfile = TagLib.File.Create(OutputDir + @"\" + metadata["title"] + metadata["fileextension"]);
+                            copiedfile.RemoveTags(TagLib.TagTypes.Id3v2);
+                            copiedfile.Tag.Title = metadata["title"];
+                            copiedfile.Tag.Performers = new string[] { metadata["artist"] };
+                            if (metadata["artwork"] != "")
+                                copiedfile.Tag.Pictures = new TagLib.Picture[] { new TagLib.Picture(metadata["artwork"]) };
+                            copiedfile.Save();
+                        }
+                        catch (Exception error)
+                        {
+                            MessageBox.Show("An error occurred while trying to copy a file! \n\n" + error.Message, "osu!export error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                        }
+
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            progressBar1.PerformStep();
+                        });
+                    }
+                    SystemSounds.Asterisk.Play();
+                }
+                else
+                {
+                    MessageBox.Show("You don't have any songs in your osu! \"Songs\" folder!", "osu!export error", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                }
+            }
+            else
+            {
+                MessageBox.Show("The osu! \"Songs\" folder was not found. \nPlease select your osu! installation directory.", "osu!export error", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+            }
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                enableGUI();
+            });
+
+        }
+
+    }
+}
